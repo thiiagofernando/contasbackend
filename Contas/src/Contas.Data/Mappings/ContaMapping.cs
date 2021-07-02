7ab602aa065ae642e4ec75e2a2ee32c837037e26
Thiago Fernando Procorio <thiago.procorio@amcom.com.br>
2021-07-02 17:13:48 -0300

    Finalizado Camada de Data

---
+++ Contas/src/Contas.Data/Mappings/ContaMapping.cs
@@ -0,0 +1,30 @@
+﻿using Contas.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Contas.Data.Mappings
+{
+    public class ContaMapping : IEntityTypeConfiguration<Conta>
+    {
+        public void Configure(EntityTypeBuilder<Conta> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome).IsRequired();
+
+            builder.Property(p => p.DiasEmAtraso).IsRequired();
+
+            builder.Property(p => p.ValorOriginal)
+                   .IsRequired()
+                   .HasColumnType("decimal(18,2)");
+
+            builder.Property(p => p.ValorCorrigido)
+                   .IsRequired()
+                   .HasColumnType("decimal(18,2)");
+
+            builder.Property(p => p.DataVencimento).IsRequired();
+
+            builder.Property(p => p.DataPagamento).IsRequired();
+        }
+    }
+}
